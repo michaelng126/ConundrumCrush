c3a1a95d5fdd4b09b5a84004b3c971e6f8f8c544
michaelng126 <michaelng126@users.noreply.github.com>
2020-06-12 21:18:38 +0100

    Finished marathon loading, using clever recursive call! Added button functions for give up and next turn.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -33,7 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   WordDatabaseHelper dbHelper;
   List<Word> _currentWord;
   String _currentDisplay = 'YXGLAA';
@@ -42,53 +40,76 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Word> _nextWord;
   bool _nextWordReady = false;
 
+  Color displayTextColor = Colors.black;
+
   @override
   void initState() {
     super.initState();
 
     dbHelper = WordDatabaseHelper.wordDbHelper;
-    firstWord().then((wordList){
-      _currentWordReady = true;
-      if (wordList == null) {
-        print('No words found. Aborting!');
-      } else {
-        setState(() {
-          String solution = wordList[0].toString();
-          print('WORD FOUND! $solution');
-          this._currentDisplay = wordList[0].displayScrambled();
-        });
-      }
+    newWord().then((wordList) {
+      _nextWordReady = true;
+      _nextWord = wordList;
+      newTurn();
     });
   }
 
-  Future<List<Word>> firstWord() async {
-    return await dbHelper.getWordandAlts(7);
-  }
-
-  // Prepares the next word.
-  void newWord() async {
-    _nextWordReady = false;
-    _nextWord = await dbHelper.getWordandAlts(7); //TODO
-    _nextWordReady = true;
+  Future<List<Word>> newWord() async {
+    return await dbHelper.getWordandAlts(9);
   }
 
   void newTurn() async {
-    _nextWord = await dbHelper.getWordandAlts(7);
     _currentWordReady = _nextWordReady;
 
-    _currentWord = _nextWord;
-    //beware of null
-    if (_currentWord == null) {
-      print('No words found. Aborting!');
-      return;
-    }
-    print('Debug statement to show currentword is passing');
-    setState(() {
-      _currentDisplay = _currentWord[0].displayScrambled();
-    });
+    if (_currentWordReady) {
+      // actually start turn
+      _currentWord = _nextWord;
+      displayTextColor = Colors.black;
 
-    //prepare next word
-    //newWord(); //TODO check this actually is async
+      if (_currentWord == null) {
+        _currentWord = [Word('No words :(')]; // TODO hack for no words
+        print('No words found. Aborting!');
+      } else {
+        setState(() {
+          String solution = _currentWord[0].toString();
+          print('WORD FOUND! $solution');
+          this._currentDisplay = _currentWord[0].displayScrambled();
+        });
+      }
+
+      // prep the next word
+      _nextWordReady = false;
+      newWord().then((wordList) {
+        _nextWordReady = true;
+        _nextWord = wordList;
+
+        if (!_currentWordReady) {
+          newTurn(); // waiting on this
+        }
+      });
+    }
+  }
+
+  void verifyAnswer(String text) {
+    bool correct = false;
+    for (var word in _currentWord) {
+      if (word.toString().toLowerCase() == text.toLowerCase()) {
+        correct = true;
+        break;
+      }
+    }
+    if (correct) {
+      setState(() {
+        displayTextColor = Colors.green;
+      });
+    }
+
+  }
+
+  void giveUp() {
+    setState(() {
+      _currentDisplay = _currentWord[0].toString().toUpperCase();
+    });
   }
 
   @override
@@ -101,44 +122,53 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Center(
           child: Column(
             children: <Widget>[
-              SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+              ),
               Text(
                 _currentDisplay,
                 style: TextStyle(
                   fontSize: 40,
                   fontFamily: 'Raleway',
+                  color: displayTextColor,
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Container(
                 width: 250.0,
-                child:TextField(
-                    decoration: InputDecoration(
-                    //border: OutlineInputBorder(),
-                    labelText: 'Answer'),
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Raleway',
-                    ),
+                child: TextField(
+                  onChanged: verifyAnswer,
+                  decoration: InputDecoration(
+                      //border: OutlineInputBorder(),
+                      labelText: 'Answer'),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Raleway',
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               RaisedButton(
-                child: new Text('Give Up',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Raleway',
-                    ),
+                child: new Text(
+                  'Give Up',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Raleway',
                   ),
-                color: Colors.lightBlueAccent,
-                textColor: Colors.black,
-                onPressed: null,
+                ),
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: giveUp,
               )
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: newTurn,
           tooltip: 'Give Up',
           child: Icon(Icons.eject),
         ),
@@ -150,6 +180,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     }
-
   }
 }
